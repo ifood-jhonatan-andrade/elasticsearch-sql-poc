e64929958da7b7c886f111dffa25a9e0c8b05ea6
Jhonatan Andrade <jhonatan.andrade@ifood.com.br>
2022-10-20 00:00:40 -0300

    feat: init poc

---
+++ src/main/kotlin/com/example/sqlelasticsearch/SqlElasticSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sqlelasticsearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SqlElasticSearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SqlElasticSearchApplication>(*args)
+}
